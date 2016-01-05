69ceb26f381d259cb7213572258614e23d69e4a6
Andrew Pariser <pariser@gmail.com>
2016-01-05 14:36:54 -0800

    Replace deprecated rspec-expectations' `:should` syntax

--- spec/delayed_job_mongo_mapper_spec.rb
+++ spec/delayed_job_mongo_mapper_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe Delayed::Backend::MongoMapper::Job do
   it_should_behave_like 'a delayed_job backend'
-  
+
   describe "before_fork" do
     after do
       ::MongoMapper.connection.close
     end
-    
+
     it "should disconnect" do
-      lambda do
+      expect(lambda do
         Delayed::Backend::MongoMapper::Job.before_fork
-      end.should change { !!MongoMapper.connection.connected? }.from(true).to(false)
+      end).to change { !!MongoMapper.connection.connected? }.from(true).to(false)
     end
   end
 
@@ -19,11 +19,11 @@ describe Delayed::Backend::MongoMapper::Job do
     before do
       ::MongoMapper.connection.close
     end
-    
+
     it "should call reconnect" do
-      lambda do
+      expect(lambda do
         Delayed::Backend::MongoMapper::Job.after_fork
-      end.should change { !!MongoMapper.connection.connected? }.from(false).to(true)
+      end).to change { !!MongoMapper.connection.connected? }.from(false).to(true)
     end
   end
 end
